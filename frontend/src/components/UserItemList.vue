32d46b19ff90103da7a5b54c1954076724d4fd0a
Alon Dai <alondai1@gmail.com>
2019-07-13 11:58:20 +0300

    fron end progress

---
+++ frontend/src/components/UserItemList.vue
@@ -0,0 +1,34 @@
+<template>
+  <section class="cards-container">
+    <h1>OnProfileItemList</h1>
+    <UserItemPreview @remove="remove" v-for="item in items" :item="item" :key="item._id"></UserItemPreview>
+  </section>
+</template>
+
+<script>
+import UserItemPreview from './UserItemPreview.vue'
+export default {
+  props: {
+    items: {
+      type: Array,
+      validator(val) {
+        return val != [];
+      }
+    },
+  },
+  methods: {
+     remove(itemId) {
+      this.$emit('remove' , itemId)
+    }
+  },
+  computed: {},
+  data() {
+    return {};
+  },
+  components: {
+    UserItemPreview
+  }
+};
+</script>
+<style lang="scss" scoped src="../styles/components/on-profile-list.scss">
+</style>
