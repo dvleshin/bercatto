966bc3dacdef63ffab75166a38452957302c90d7
Alon Dai <alondai1@gmail.com>
2019-07-14 15:06:26 +0300

    filter progress

--- frontend/src/components/FilterSection.vue
+++ frontend/src/components/FilterSection.vue
@@ -1,34 +1,33 @@
 <template>
   <form @submit.prevent="emitFilterBy">
-      <v-text-field v-model="filterBy.txt" label="Items to trade"></v-text-field>
-      <v-select :items="sortItems" v-model="filterBy.sortBy" label="Sort by"></v-select>
-      <v-btn color="blue" type="submit">Search</v-btn>
-      <v-btn color="red" @click="clearForm" fab small><v-icon>clear</v-icon></v-btn>
-    </form>
+    <v-text-field v-model="filterBy.txt" label="Items to trade"></v-text-field>
+    <v-select :items="['new', 'used']" v-model="filterBy.type" label="Condition"></v-select>
+    <v-select :items="sortItems" v-model="filterBy.sortBy" label="Sort by"></v-select>
+    <v-btn color="blue" type="submit">Search</v-btn>
+    <v-btn color="red" @click="clearForm" fab small>
+      <v-icon>clear</v-icon>
+    </v-btn>
+  </form>
 </template>
 
 <script>
 export default {
-  
   methods: {
     clearForm() {
-      this.filterBy = {}
+      this.filterBy = {};
     },
     emitFilterBy() {
-console.log(this.filterBy);
-
+      console.log(this.filterBy);
     }
   },
   computed: {},
   data() {
     return {
-      filterBy:{},
+      filterBy: {},
       sortItems: ["Value", "Views"]
     };
   },
-  components: {
-   
-  }
+  components: {}
 };
 </script>
 <style lang="scss" scoped src="../styles/components/filter-section.scss">
