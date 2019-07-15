36e38017b774ce680ff2cc53b8c5ae20d0629599
Shay Cohen <shaycohen84@gmail.com>
2019-07-15 13:26:18 +0300

    minor changes

--- frontend/src/components/Header.vue
+++ frontend/src/components/Header.vue
@@ -1,43 +1,27 @@
 <template>
   <v-toolbar>
     <v-toolbar-items v-if="!loggedInUser" class="hidden-sm-and-down">
-      <v-text-field
-                  label="User Name"
-                  v-model="userName"
-                  :rules="userNameRules"
-                  required
-                  
-                ></v-text-field>
-      
-    <v-text-field
-                  label="Password"
-                  v-model="Password"
-                  :rules="passwordRules"
-                  required
-                  
-                ></v-text-field>
+      <v-text-field label="User Name" v-model="userName" :rules="userNameRules" required></v-text-field>
 
-                <v-avatar
-          :tile="tile"
-          :size="avatarSize"
-          color="grey lighten-4"
-          v-if="loggedInUser"
-        >
-          <img :src="loggedInUser.profileImg">
-        </v-avatar>
-      
+      <v-text-field label="Password" v-model="Password" :rules="passwordRules" required></v-text-field>
+      <v-btn
+        @click="submit"
+        :class=" { 'blue darken-4 white--text' : valid, disabled: !valid }"
+      >LogIn</v-btn>
+      <v-avatar :tile="tile" :size="avatarSize" color="grey lighten-4" v-if="loggedInUser">
+        <img :src="loggedInUser.profileImg" />
+      </v-avatar>
     </v-toolbar-items>
   </v-toolbar>
 </template>
 
 <script>
 export default {
- created(){
-   this.loggedInUser = JSON.parse(sessionStorage.getItem('loggedInUser').user)
- }
+  created() {
+    this.loggedInUser = JSON.parse(sessionStorage.getItem("loggedInUser").user);
+  }
 };
 </script>
 
 <style lang="scss" scoped src="../styles/components/header.scss">
-
 </style>
