b58903dff789a83d746094f67076a1bf701d0158
Alon Dai <alondai1@gmail.com>
2019-07-18 08:52:26 +0300

    header changes

--- frontend/src/components/Header.vue
+++ frontend/src/components/Header.vue
@@ -10,9 +10,11 @@
       <div v-if="loggedInUser" class="user-menu">
         <v-icon @click="addItem">add</v-icon>
         arenas: ({{loggedInUser.arenas.length}})
+
         <v-avatar size="60px">
           <img :src="loggedInUser.profileImg" />
         </v-avatar>
+        <button @click="go">GO</button>
         <v-icon @click.prevent="onLogOut" color="#fff" size="30px">exit_to_app</v-icon>
       </div>
       <div class="login-area" v-if="!loggedInUser">
@@ -84,6 +86,9 @@ export default {
     };
   },
   methods: {
+    go() {
+      this.$router.push(this.loggedInUser.arenas[0].url)
+    },
     goMain() {
       this.$router.push("/");
     },
