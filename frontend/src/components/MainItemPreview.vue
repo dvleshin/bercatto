4d2c585664adee82d7a42da19977509fd5d05f98
Shay Cohen <shaycohen84@gmail.com>
2019-07-19 11:30:56 +0300

    starred reviews

--- frontend/src/components/MainItemPreview.vue
+++ frontend/src/components/MainItemPreview.vue
@@ -40,7 +40,7 @@ export default {
   },
   data() {
     return {
-      moment:moment,
+      moment,
       loggedInUser:null
     }
   },
--- frontend/src/components/UserItemPreview.vue
+++ frontend/src/components/UserItemPreview.vue
@@ -3,13 +3,19 @@
       <img :src="item.imgUrl[0]" alt="">
       <div class="info-container">
           <h1>{{item.name}}</h1>
-          <p>{{item.description}}</p>
+          <p>{{txtToShow}}</p>
       </div>
     </div>
 </template>
 <script>
 export default {
-    props: ['item']
+    props: ['item'],
+    computed: {
+    txtToShow(){
+            if (this.item.description.length <= 100) return this.item.description
+            return this.item.description.substr(0, 100) + '...'
+        },
+  }
 }
 </script>
 <style lang="scss" scoped src="../styles/components/on-profile-preview.scss">
